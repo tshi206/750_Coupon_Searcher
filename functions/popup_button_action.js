48c19230b765b657631a152e5cb83b2bc1a974c3
tshi206 <tshi206@aucklanduni.ac.nz>
2018-04-29 20:43:57 +1200

    finish search features

--- functions/popup_button_action.js
+++ functions/popup_button_action.js
@@ -1,18 +1,40 @@
 $().ready( () => {
     $("#searchBtn").on("click", () => {
         console.log("search button clicked");
-        // todo - send ajax to coupon api
-        $.ajax({ url: 'https://www.amazon.com/coupons/b/?node=13285418011', success: function(data) { console.log(data); } });
         let searchText = $('#searchField').val();
-        chrome.windows.create({type: "popup", url: "https://www.amazon.com/s/ref=nb_sb_noss?url=srs%3D2231352011%26search-alias%3Dcoupons&field-keywords=" + searchText, width: 1024, height: 600}, (window) => {
-            console.log(window.id);
-            console.log("window created");
+        chrome.storage.sync.get(['history'], (obj) => {
+            let histories = obj.history ? obj.history : [];
+            histories.push(searchText); // this is used as a stack, to retrieve items use array.pop();
+            console.log(histories); // use it for debugging if needed
+            chrome.storage.sync.set({history: histories}, () => {
+                chrome.windows.create({type: "popup",
+                    url: "https://www.amazon.com/s/ref=nb_sb_noss?url=srs%3D2231352011%26search-alias%3Dcoupons&field-keywords="
+                    + searchText, width: 1024, height: 600}, (window) => {
+                    console.log(window.id);
+                    console.log("window created");
+                });
+            });
+        });
+    });
+
+    $('.list-group-item').on("click", () => {
+        let item = this.document.activeElement;
+        console.log("history button clicked");
+        console.log(item);
+        let searchText = $(item).text();
+        console.log(searchText);
+        chrome.storage.sync.get(['history'], (obj) => {
+            let histories = obj.history ? obj.history : [];
+            histories.push(searchText); // this is used as a stack, to retrieve items use array.pop();
+            console.log(histories); // use it for debugging if needed
+            chrome.storage.sync.set({history: histories}, () => {
+                chrome.windows.create({type: "popup",
+                    url: "https://www.amazon.com/s/ref=nb_sb_noss?url=srs%3D2231352011%26search-alias%3Dcoupons&field-keywords="
+                    + searchText, width: 1024, height: 600}, (window) => {
+                    console.log(window.id);
+                    console.log("window created");
+                });
+            });
         });
     })
 } );
-
-function clickSearchBtn() {
-    console.log("I'm fired");
-    // todo - fill up the search bar
-    $("#searchBtn").click();
-}
